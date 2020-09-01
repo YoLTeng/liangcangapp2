0ea5c29ed2b6c89027d50185ba73c8eb261815ee
y777yx <18370097423@163.com>
2020-09-01 21:05:45 +0800

    第一天首页开发

--- src/main.js
+++ src/main.js
@@ -9,11 +9,16 @@ import "./assets/style/reset.css"
 import 'amfe-flexible'
 
 //按需引入组件
-import {Button,Tabbar, TabbarItem} from "vant";
+import {Button,Tabbar, TabbarItem, Search, Tab, Tabs, Swipe, SwipeItem } from "vant";
 
 Vue.use(Button)
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
+Vue.use(Search);
+Vue.use(Tab);
+Vue.use(Tabs);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
 
 
 Vue.config.productionTip = false
