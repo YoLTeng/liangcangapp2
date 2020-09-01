87e4d6e01ed0551d9fdbbc02b7f6bb2e05390074
foina-317 <zf1244957087@126.com>
2020-09-01 21:06:05 +0800

    20200901详情页第一次提交，静态页面，暂无功能

--- src/main.js
+++ src/main.js
@@ -9,17 +9,30 @@ import "./assets/style/reset.css"
 import 'amfe-flexible'
 
 //按需引入组件
-import {Button,Tabbar, TabbarItem} from "vant";
+import {
+    Button,
+    Tabbar,
+    TabbarItem,
+    Swipe,
+    SwipeItem,
+    NavBar,
+    Cell
+
+} from "vant";
 
 Vue.use(Button)
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
-
+//详情页引入
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+Vue.use(NavBar);
+Vue.use(Cell);
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
