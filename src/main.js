5d49f161edfb750701a4ce06979d9d3299930f92
your-babaa <yclong1109@163.com>
2020-09-01 21:24:33 +0800

    yuchenglong-20.9.1-add better-scroll state vant

--- src/main.js
+++ src/main.js
@@ -1,25 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 //引入css重置代码
-import "./assets/style/reset.css"
+import "./assets/style/reset.css";
 
 // 引入适配rem文件
-import 'amfe-flexible'
+import "amfe-flexible";
 
 //按需引入组件
-import {Button,Tabbar, TabbarItem} from "vant";
+import { Button, Tabbar, TabbarItem, Icon, Lazyload } from "vant";
 
-Vue.use(Button)
+Vue.use(Button);
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
+Vue.use(Icon);
+Vue.use(Lazyload);
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,39 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
 //  解构赋值引入api.js里的方法
 //例子 import {} from "../utils/api";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    //state例子
-    //baninfo:""
-    tjs:"",
-  },
-  mutations: {
-    //mutation例子
-    // getbann(state,payload){
-    //   state.baninfo = payload.info
-    // },
-  },
-  actions: {
-    // 请求接口例子
-    //getBannerinfo---actions的方法名，payload参数
-    //await---后面的getBannerinfo，为api.js里的方法名
-    //getbann---mutations里面的方法名
+    state: {
+        //state例子
+        //baninfo:""
+        tjs: "",
+        moveMagazine: false,
+    },
+    mutations: {
+        //mutation例子
+        // getbann(state,payload){
+        //   state.baninfo = payload.info
+        // },
 
-    // async getBannerinfo({commit},payload){
-    //   const data = await getBannerinfo();
-    //   commit('getbann',{
-    //     info:data.result.list
-    //   })
-    // },
-  },
-  modules: {
-  }
-})
+        ifmove(state) {
+            state.moveMagazine = !state.moveMagazine;
+        },
+    },
+    actions: {
+        // 请求接口例子
+        //getBannerinfo---actions的方法名，payload参数
+        //await---后面的getBannerinfo，为api.js里的方法名
+        //getbann---mutations里面的方法名
+        // async getBannerinfo({commit},payload){
+        //   const data = await getBannerinfo();
+        //   commit('getbann',{
+        //     info:data.result.list
+        //   })
+        // },
+    },
+    modules: {},
+});
