f4277592ed568c77d294fb10c86ba8462042d24a
2311842283 <2311842283@qq.com>
2020-09-02 23:01:24 +0800

    9.02第二次提交，商品列表

--- src/views/Shoplist.vue
+++ src/views/Shoplist.vue
@@ -20,25 +20,30 @@
         {{ item.title }}
       </li>
     </ul>
-
+    <!-- 商品内容 -->
     <van-swipe
       class="my-swipe"
       :loop="false"
       :show-indicators="false"
       ref="changebanner"
+      @change="onChange"
     >
-      <van-swipe-item>
-        <div class="list-wrap">
+      <van-swipe-item v-for="item in 6" :key="item">
+        <div class="list-wrap" >
           <ul>
             <li v-for="item in 10" :key="item">
-              {{ item }}
+              <img src="../assets/findimg/img4.png" alt="" />
+              <div class="shop-info">
+                <p class="title">全身按摩床垫升级款</p>
+                <p class="effect">升级颈椎揉捏加大热敷面积</p>
+                <p class="sketch">颈部正反按摩 要被大面积热敷 海燕包赠送</p>
+                <p class="price">￥999</p>
+              </div>
             </li>
           </ul>
         </div>
       </van-swipe-item>
-      <van-swipe-item>2asdasd</van-swipe-item>
-      <van-swipe-item>3</van-swipe-item>
-      <van-swipe-item>4</van-swipe-item>
+     
     </van-swipe>
   </div>
 </template>
@@ -65,14 +70,14 @@ export default {
           title: "玩乐",
           id: "4",
         },
-        // {
-        //   title: "餐厨",
-        //   id: "5",
-        // },
-        // {
-        //   title: "美食",
-        //   id: "6",
-        // },
+        {
+          title: "餐厨",
+          id: "5",
+        },
+        {
+          title: "美食",
+          id: "6",
+        },
         // {
         //   title: "服装",
         //   id: "7",
@@ -99,9 +104,12 @@ export default {
     },
     //点击切换商品列表
     tabClick(i) {
-      this.isActive = i;
+      // this.isActive = i;
       this.$refs.changebanner.swipeTo(i);
     },
+    onChange(i){
+      this.isActive = i;
+    }
   },
 };
 </script>
@@ -127,14 +135,68 @@ export default {
       margin: 0 16px 0 16px;
     }
     .active {
-      color: #ff9180;
+      color: #171717;
       border-bottom: 3px solid;
     }
   }
-  .my-swipe .van-swipe-item {
-    font-size: 20px;
-    line-height: 150px;
-    text-align: center;
+
+  //商品列表
+  .van-swipe {
+    margin-top: 46px;
+    .van-swipe-item {
+      .list-wrap {
+        ul {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+          padding: 14px 17px 0 17px;
+          li {
+            height: 300px;
+            width: 166px;
+            background: #f0f0f0;
+            margin-bottom: 7px;
+            img {
+              width: 166px;
+              height: 165px;
+            }
+            .shop-info {
+              width: 100%;
+              height: 134px;
+              display: flex;
+              flex-direction: column;
+              font-size: 11px;
+              padding-left: 6px;
+              .title {
+                font-size: 13px;
+                color: #1e1e1e;
+                height: 20px;
+                line-height: 34px;
+              }
+              .effect {
+                height: 23px;
+                line-height: 28px;
+                color: #1e1e1e;
+                font-weight: 400;
+                margin-bottom: 5px;
+              }
+              .sketch {
+                height: 34px;
+                font-weight: 400;
+                color: #939393;
+                line-height: 17px;
+              }
+              .price {
+                margin-top: 8px;
+                font-size: 15px;
+                font-family: PingFang SC;
+                font-weight: 600;
+                color: #7292bc;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
