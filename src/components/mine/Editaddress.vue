3c1efdfb301daa59272c8c4a9d492fc3410d0bd6
YoLTeng <1971369811@qq.com>
2020-09-07 15:10:01 +0800

    上传购物车页面，地址管理页面，功能，静态页面都已完成

--- src/components/mine/Editaddress.vue
+++ src/components/mine/Editaddress.vue
@@ -2,18 +2,64 @@
   <div class="editaddress">
     <!-- 顶部 -->
     <van-nav-bar title="编辑收货地址" left-arrow @click-left="onClickLeft" />
+     <van-form>
+      <van-field
+        v-model="username"
+        placeholder="收件人真实姓名"
+        :rules="[{ required: true }]"
+      />
+      <van-field
+        v-model="tel"
+        placeholder="手机号"
+        :rules="[{ required: true }]"
+      />
+      <van-field
+        readonly
+        clickable
+        name="area"
+        :value="value"
+        placeholder="收货地址"
+        @click="showArea = true"
+        
+      />
+      <van-field
+        v-model="describe"
+        placeholder="详细地址"
+        :rules="[{ required: true }]"
+      />
+      
+      <van-popup v-model="showArea" position="bottom">
+        <van-area :title="value" :area-list="areaList" @confirm="onConfirm" @cancel="showArea = false" />
+      </van-popup>
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit" :square="true" color="#000">保存</van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
 <script>
+import city from "../../assets/js/city";
+
 export default {
   data() {
-    return {};
+    return {
+      value: "",
+      showArea: false,
+      areaList: city, // 数据格式见 Area 组件文档
+      username: "",
+      tel: "",
+      describe: "",
+    };
   },
 
   methods: {
     onClickLeft() {
       this.$router.go(-1);
+    },
+    onConfirm(values) {
+      this.value = values.map((item) => item.name).join("  ");
+      this.showArea = false;
     },
   },
 };
@@ -21,9 +67,28 @@ export default {
 
 
 <style lang="scss">
-.van-nav-bar {
+.editaddress{
+  .van-nav-bar {
   .van-icon-arrow-left {
     color: #000;
   }
 }
+form {
+  width: 100%;
+  height: 384px;
+  padding: 34px 23px;
+  background: #fafafa;
+  display: flex;
+  flex-direction: column;
+  .van-cell__value {
+    margin-top: 18px;
+    height: 64px;
+  }
+   .van-button {
+      width: 288px;
+      height: 54px;
+    }
+ }
+}
+
 </style>
